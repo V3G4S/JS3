9f0e49181e59d609085193d5dc5fd8d110799a58
Antony Valentim <162443432+V3G4S@users.noreply.github.com>
2024-07-10 08:44:44 -0300

    Update ex7.js

---
+++ ex7.js
@@ -0,0 +1,5 @@
+function mediaNota(){
+    a = Number(prompt("Qual a primeira nota?")), b = Number(prompt("Qual a segunda nota?")), c = Number(prompt("Qual a terceira nota?"))
+    let media = (a + b + c) / 3
+    alert(media)
+}
